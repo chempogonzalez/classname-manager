667af5b4fd5f23b9addc04a88876ae3519796ded
chempogonzalez <manugonzalezfranco@gmail.com>
2022-12-01 20:55:03 +0100

    feat: improve type-safe inference + tsdocs

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,43 @@
 
 type ClassName = string | Array<string>
 
+type PureType<T, Type> = T extends (Type | undefined) ? Type : T extends Type ? Type : T
 
-type ToBoolean<T> = T extends string
-  ? T extends 'true'
-    ? true
-    : false
-  : never
+type PureString<T> = PureType<T, string>
+type PureBoolean<T> = PureType<T, boolean>
 
-interface Schema<Props> {
-  base: ClassName
-  dynamicVariants?: {
-    [propsKey in keyof Props]?: Props[propsKey] extends string
-    // String or string union ('primary' | 'secondary')
-      ? {
-          [strValue in Props[propsKey]]?: {
-            [propValueKey in strValue]: ClassName
-          } & { DEFAULT?: strValue }
-        }[Props[propsKey]]
 
-    // Boolean case
-      : Props[propsKey] extends boolean
-        ? {
-            [strValue in 'true' | 'false']?: {
-              [propValueKey in strValue]: ClassName
-            } & { DEFAULT?: ToBoolean<strValue> }
-          }['true' | 'false']
-        : never
-  }
-  dynamicClassNames?: {
-    [key: string]: boolean
-  }
+
+type OmitUndef<T> = T extends (string | undefined) ? Exclude<T, undefined> : T extends string ? T : never
+
+
+type Schema<Props> = {
+	base?: ClassName
+	dynamicVariants?: {
+		[propsKey in keyof Props]?: PureString<Props[propsKey]> extends string
+		// String or string union ('primary' | 'secondary')
+			? {
+					[strValue in OmitUndef<Props[propsKey]>]?: {
+						[propValueKey in strValue]: ClassName
+					} & { DEFAULT?: Props[propsKey] }
+				}[OmitUndef<Props[propsKey]>]
+
+		// Boolean case
+			: PureBoolean<Props[propsKey]> extends boolean
+				? {
+						[strValue in 'true' | 'false']?: {
+							[propValueKey in strValue]: ClassName
+						} & { DEFAULT?: boolean }
+					}['true' | 'false']
+
+			// Rest of cases
+				: {
+						[variantValue: string]: string
+					}
+	}
+	dynamicClassNames?: {
+		[key: string]: boolean
+	}
 }
 
 type SchemaFunction<T> = (props: T & { extraClassNames?: ClassName }) => string
@@ -38,23 +45,78 @@ type SchemaFunction<T> = (props: T & { extraClassNames?: ClassName }) => string
 
 
 
-
 /**
+ *	Returns a function which allows you to generate the class name string
+ *	based on the schema and props passed to it.
  *
- * @param schemaWithProps
+ * @param schemaInitializer - A function which returns a schema object and accepts props as an argument.
+ *
+ * @example
+ *
+ * ```ts
+ *
+ *	// Define a schema based on props
+ *	const getClassName = classNameManager(props => ({
+ *		base: 'base-class',
+ *		dynamicVariants: {
+ *			variant: {
+ *				DEFAULT: 'primary' // Point to the primary variant by default
+ *				primary: 'primary-variant',
+ *				secondary: 'secondary-variant',
+ *			}
+ *		},
+ *		dynamicClassNames: {
+ *			'is-active': props.isActive
+ *		}
+ * 	}))
+ *
+ * const buttonClassName = getClassName({ variant: 'secondary', isActive: true })
+ *
+ * // Returns: 'base-class secondary-variant is-active'
+ *
+ *
+ * ```
  */
 export function classNameManager <T> (
-  schemaWithProps: (props: T) => Schema<T>
+	schemaInitializer: (props: T) => Schema<T>
 ): SchemaFunction<T>
 
 
 /**
  *
- * @param schema
+ * Returns a function which allows you to generate the class name string
+ * based on the schema and props passed to it.
+ *
+ * @param schema - The schema object.
+ *
+ * @example
+ *
+ * ```ts
+ *
+ *	// Define a schema based on props
+ *	const getClassName = classNameManager({
+ *		base: 'base-class',
+ *		dynamicVariants: {
+ *			styleType: {
+ *				DEFAULT: 'primary' // Point to the primary variant by default
+ *				primary: 'primary-variant',
+ *				secondary: 'secondary-variant',
+ *			}
+ *		},
+ *	})
+ *
+ * const buttonClassName = getClassName({ styleType: 'secondary', isActive: true })
+ *
+ * // Returns: 'base-class secondary-variant is-active'
+ *
+ *
+ * ```
  */
-export function classNameManager <T> (
-  schema: Schema<T>
+export function classNameManager <T extends Record<string, unknown>> (
+	schema: Schema<T>
 ): SchemaFunction<T>
+
+
 
 
 
@@ -62,36 +124,36 @@ export function classNameManager <T> (
  * @internal
  */
 export function classNameManager <T> (
-  schemaOrFunc: ((props: T) => Schema<T>) | Schema<T>,
+	schemaOrFunc: ((props: T) => Schema<T>) | Schema<T> = {} as unknown as ((props: T) => Schema<T>) | Schema<T>,
 ): SchemaFunction<T> {
-  return (componentProps: any) => {
-    const schemaObject = typeof schemaOrFunc === 'function'
-      ? schemaOrFunc(componentProps)
-      : schemaOrFunc
+	return (componentProps: any) => {
+		const schemaObject = typeof schemaOrFunc === 'function'
+			? schemaOrFunc(componentProps)
+			: schemaOrFunc
 
-    const { extraClassNames } = componentProps
-    const { base, dynamicVariants, dynamicClassNames } = schemaObject
+		const { extraClassNames } = componentProps
+		const { base, dynamicVariants, dynamicClassNames } = schemaObject
 
-    const classNameArray = [base ?? '']
+		const classNameArray = [base ?? '']
 
-    if (dynamicVariants) {
-      Object.entries(dynamicVariants).forEach(([key, value]: any) => {
-        const propValue = componentProps[key]
-        const schemaValue = value[propValue] ?? value[value.DEFAULT]
-        schemaValue && classNameArray.push(schemaValue)
-      })
-    }
+		if (dynamicVariants) {
+			Object.entries(dynamicVariants).forEach(([key, value]: any) => {
+				const propValue = componentProps[key]
+				const schemaValue = value[propValue] ?? value[value.DEFAULT]
+				schemaValue && classNameArray.push(schemaValue)
+			})
+		}
 
-    if (dynamicClassNames) {
-      Object.entries(dynamicClassNames).forEach(([key, value]) => {
-        value && classNameArray.push(key)
-      })
-    }
+		if (dynamicClassNames) {
+			Object.entries(dynamicClassNames).forEach(([key, value]) => {
+				value && classNameArray.push(key)
+			})
+		}
 
-    if (extraClassNames) classNameArray.push(extraClassNames)
+		if (extraClassNames) classNameArray.push(extraClassNames)
 
-    return classNameArray.flat(4).join(' ').trim()
-  }
+		return classNameArray.flat(4).join(' ').trim()
+	}
 }
 
 export const cnm = classNameManager
