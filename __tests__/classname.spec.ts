6ec4c861b6407c7000a5b8016b7286f3fe34ab86
chempogonzalez <manugonzalezfranco@gmail.com>
2023-05-30 03:24:23 +0200

    fix: solve problem with types inference

--- __tests__/classname.spec.ts
+++ __tests__/classname.spec.ts
@@ -5,160 +5,160 @@ import { classNameManager } from '../src'
 
 
 type ButtonProps = {
-	styleType?: 'primary' | 'secondary'
-	isEnabled?: boolean
+  styleType?: 'primary' | 'secondary'
+  isEnabled?: boolean
 }
 
 describe('<< classNameManager >>', () => {
-	describe('------------------- [base] --------------------', () => {
-		it('- is filled', () => {
-			const result = 'btn-base'
+  describe('------------------- [base] --------------------', () => {
+    it('- is filled', () => {
+      const result = 'btn-base'
 
-			const getClassName = classNameManager<ButtonProps>({
-				base: 'btn-base',
-			})
+      const getClassName = classNameManager<ButtonProps>({
+        base: 'btn-base',
+      })
 
-			const className = getClassName({ styleType: 'primary' })
-			expect(className).toBe(result)
-		})
+      const className = getClassName({ styleType: 'primary' })
+      expect(className).toBe(result)
+    })
 
-		it('- not filled', () => {
-			const result = ''
-
-
-			const getClassNameEmptyObj = classNameManager<ButtonProps>({})
-			/* @ts-expect-error - undefined is not allowed */
-			const getClassNameUndefined = classNameManager<ButtonProps>(undefined)
-			/* @ts-expect-error - expect 1 argument and is 0 */
-			const getClassNameNoArgs = classNameManager<ButtonProps>()
+    it('- not filled', () => {
+      const result = ''
 
 
-			const className = getClassNameEmptyObj({ styleType: 'primary' })
-			const className1 = getClassNameUndefined({ styleType: 'primary' })
-			const className2 = getClassNameNoArgs({ styleType: 'primary' })
-
-			expect(className).toBe(result)
-			expect(className1).toBe(result)
-			expect(className2).toBe(result)
-		})
-	})
+      const getClassNameEmptyObj = classNameManager<ButtonProps>({})
+      /* @ts-expect-error - undefined is not allowed */
+      const getClassNameUndefined = classNameManager<ButtonProps>(undefined)
+      /* @ts-expect-error - expect 1 argument and is 0 */
+      const getClassNameNoArgs = classNameManager<ButtonProps>()
 
 
-	describe('--------------- [dynamicVariants] -------------', () => {
-		it('- is filled', () => {
-			const result = 'btn-base btn-primary'
+      const className = getClassNameEmptyObj({ styleType: 'primary' })
+      const className1 = getClassNameUndefined({ styleType: 'primary' })
+      const className2 = getClassNameNoArgs({ styleType: 'primary' })
 
-			const getClassName = classNameManager<ButtonProps>({
-				base: 'btn-base',
-				dynamicVariants: {
-					styleType: {
-						DEFAULT: 'primary',
-						primary: 'btn-primary',
-						secondary: 'btn-secondary',
-					},
-				},
-			})
-
-			const className = getClassName({ styleType: 'primary' })
-			expect(className).toBe(result)
-		})
-
-		it('- is filled with boolean', () => {
-			const result = 'btn-base btn-primary btn-enabled'
-
-			const getClassName = classNameManager<ButtonProps>({
-				base: 'btn-base',
-				dynamicVariants: {
-					styleType: {
-						primary: 'btn-primary',
-						secondary: 'btn-secondary',
-					},
-					isEnabled: {
-						DEFAULT: true,
-						true: 'btn-enabled',
-						false: '',
-					},
-				},
-			})
-
-			const className = getClassName({ styleType: 'primary', isEnabled: true })
-			expect(className).toBe(result)
-		})
-
-		it('- is filled with array', () => {
-			const result = 'btn-base btn-primary btn-primary-2 btn-enabled btn-enabled-2'
-
-			const getClassName = classNameManager<ButtonProps>({
-				base: 'btn-base',
-				dynamicVariants: {
-					styleType: {
-						primary: [
-							'btn-primary',
-							'btn-primary-2',
-						],
-						secondary: 'btn-secondary',
-					},
-					isEnabled: {
-						DEFAULT: true,
-						true: ['btn-enabled', 'btn-enabled-2'],
-						false: '',
-					},
-				},
-			})
-
-			const className = getClassName({ styleType: 'primary', isEnabled: true })
-			expect(className).toBe(result)
-		})
-
-		it('- not valid values', () => {
-			const result = ''
-
-			const getClassNameEmptyObj = classNameManager<ButtonProps>({ dynamicVariants: {} })
-
-			const getClassNameUndefined = classNameManager<ButtonProps>({ dynamicVariants: undefined })
-			/* @ts-expect-error - Is not a string */
-			const getClassNameNoArgs = classNameManager<ButtonProps>({ dynamicVariants: '' })
+      expect(className).toBe(result)
+      expect(className1).toBe(result)
+      expect(className2).toBe(result)
+    })
+  })
 
 
-			const className = getClassNameEmptyObj({ styleType: 'primary' })
-			const className1 = getClassNameUndefined({ styleType: 'primary' })
-			const className2 = getClassNameNoArgs({ styleType: 'primary' })
+  describe('--------------- [dynamicVariants] -------------', () => {
+    it('- is filled', () => {
+      const result = 'btn-base btn-primary'
 
-			expect(className).toBe(result)
-			expect(className1).toBe(result)
-			expect(className2).toBe(result)
-		})
-	})
+      const getClassName = classNameManager<ButtonProps>({
+        base: 'btn-base',
+        dynamicVariants: {
+          styleType: {
+            DEFAULT: 'primary',
+            primary: 'btn-primary',
+            secondary: 'btn-secondary',
+          },
+        },
+      })
+
+      const className = getClassName({ styleType: 'primary' })
+      expect(className).toBe(result)
+    })
+
+    it('- is filled with boolean', () => {
+      const result = 'btn-base btn-primary btn-enabled'
+
+      const getClassName = classNameManager<ButtonProps>({
+        base: 'btn-base',
+        dynamicVariants: {
+          styleType: {
+            DEFAULT: 'primary',
+            primary: 'btn-primary',
+            secondary: 'btn-secondary',
+          },
+          isEnabled: {
+            DEFAULT: true,
+            true: '',
+          },
+        },
+      })
+
+      const className = getClassName({ styleType: 'primary', isEnabled: true })
+      expect(className).toBe(result)
+    })
+
+    it('- is filled with array', () => {
+      const result = 'btn-base btn-primary btn-primary-2 btn-enabled btn-enabled-2'
+
+      const getClassName = classNameManager<ButtonProps>({
+        base: 'btn-base',
+        dynamicVariants: {
+          styleType: {
+            primary: [
+              'btn-primary',
+              'btn-primary-2',
+            ],
+            secondary: 'btn-secondary',
+          },
+          isEnabled: {
+            DEFAULT: true,
+            true: ['btn-enabled', 'btn-enabled-2'],
+            false: '',
+          },
+        },
+      })
+
+      const className = getClassName({ styleType: 'primary', isEnabled: true })
+      expect(className).toBe(result)
+    })
+
+    it('- not valid values', () => {
+      const result = ''
+
+      const getClassNameEmptyObj = classNameManager<ButtonProps>({ dynamicVariants: {} })
+
+      const getClassNameUndefined = classNameManager<ButtonProps>({ dynamicVariants: undefined })
+      /* @ts-expect-error - Is not a string */
+      const getClassNameNoArgs = classNameManager<ButtonProps>({ dynamicVariants: '' })
 
 
-	describe('-------------- [dynamicClassNames] -------------', () => {
-		it('- is filled', () => {
-			const result = 'btn-base dynamic-class'
+      const className = getClassNameEmptyObj({ styleType: 'primary' })
+      const className1 = getClassNameUndefined({ styleType: 'primary' })
+      const className2 = getClassNameNoArgs({ styleType: 'primary' })
 
-			const getClassName = classNameManager<ButtonProps>(props => ({
-				base: 'btn-base',
-				dynamicClassNames: {
-					'dynamic-class': props.styleType === 'primary',
-				},
-			}))
+      expect(className).toBe(result)
+      expect(className1).toBe(result)
+      expect(className2).toBe(result)
+    })
+  })
 
-			const className = getClassName({ styleType: 'primary' })
-			expect(className).toBe(result)
-		})
-	})
-	describe('-------------- [extraClassNames] ---------------', () => {
-		it('- is filled', () => {
-			const result = 'btn-base dynamic-class extra-class'
 
-			const getClassName = classNameManager<ButtonProps>(props => ({
-				base: 'btn-base',
-				dynamicClassNames: {
-					'dynamic-class': props.styleType === 'primary',
-				},
-			}))
+  describe('-------------- [dynamicClassNames] -------------', () => {
+    it('- is filled', () => {
+      const result = 'btn-base dynamic-class'
 
-			const className = getClassName({ styleType: 'primary', extraClassNames: 'extra-class' })
-			expect(className).toBe(result)
-		})
-	})
+      const getClassName = classNameManager<ButtonProps>(props => ({
+        base: 'btn-base',
+        dynamicClassNames: {
+          'dynamic-class': props.styleType === 'primary',
+        },
+      }))
+
+      const className = getClassName({ styleType: 'primary' })
+      expect(className).toBe(result)
+    })
+  })
+  describe('-------------- [extraClassNames] ---------------', () => {
+    it('- is filled', () => {
+      const result = 'btn-base dynamic-class extra-class'
+
+      const getClassName = classNameManager<ButtonProps>(props => ({
+        base: 'btn-base',
+        dynamicClassNames: {
+          'dynamic-class': props.styleType === 'primary',
+        },
+      }))
+
+      const className = getClassName({ styleType: 'primary', extraClassNames: 'extra-class' })
+      expect(className).toBe(result)
+    })
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+// eslint-disable-next-line @typescript-eslint/ban-types
+} & {}
+
+
 type ClassName = string | Array<string>
 
 type PureType<T, Type> = T extends (Type | undefined) ? Type : T extends Type ? Type : T
@@ -7,47 +13,48 @@ type PureString<T> = PureType<T, string>
 type PureBoolean<T> = PureType<T, boolean>
 
 
-
 type OmitUndef<T> = T extends (string | undefined) ? Exclude<T, undefined> : T extends string ? T : never
 
 
-type Schema<Props> = {
-	base?: ClassName
-	dynamicVariants?: {
-		[propsKey in keyof Props]?: PureString<Props[propsKey]> extends string
-		// String or string union ('primary' | 'secondary')
-			? {
-					[strValue in OmitUndef<Props[propsKey]>]?: {
-						[propValueKey in strValue]: ClassName
-					} & { DEFAULT?: Props[propsKey] }
-				}[OmitUndef<Props[propsKey]>]
+type Schema<Props extends Record<string, unknown>> = {
+  base?: ClassName
+  dynamicVariants?: Prettify<{
+    [propsKey in keyof Props]?: PureString<Props[propsKey]> extends string
+      // String or string union ('primary' | 'secondary')
+      ? Prettify<{
+        [strValue in OmitUndef<Props[propsKey]>]?: {
+          [propValueKey in strValue]: ClassName
+        }[strValue]
+      } & { DEFAULT?: Props[propsKey] }>
 
-		// Boolean case
-			: PureBoolean<Props[propsKey]> extends boolean
-				? {
-						[strValue in 'true' | 'false']?: {
-							[propValueKey in strValue]: ClassName
-						} & { DEFAULT?: boolean }
-					}['true' | 'false']
+      // Boolean case
+      : PureBoolean<Props[propsKey]> extends boolean
+        ? Prettify<{
+          [strValue in 'true' | 'false']?: {
+            [propValueKey in strValue]: ClassName
+          }[strValue]
+        } & { DEFAULT?: boolean }>
 
-			// Rest of cases
-				: {
-						[variantValue: string]: string
-					}
-	}
-	dynamicClassNames?: {
-		[key: string]: boolean
-	}
+        // Rest of cases
+        : {
+            [variantValue: string]: string
+          }
+  }>
+  dynamicClassNames?: {
+    [key: string]: boolean
+  }
 }
 
-type SchemaFunction<T> = (props: T & { extraClassNames?: ClassName }) => string
+type SchemaFunction<T extends Record<string, unknown>> = (
+  props: Prettify<T & { extraClassNames?: ClassName }>
+) => string
 
 
 
 
 /**
- *	Returns a function which allows you to generate the class name string
- *	based on the schema and props passed to it.
+ * Returns a function which allows you to generate the class name string
+ * based on the schema and props passed to it.
  *
  * @param schemaInitializer - A function which returns a schema object and accepts props as an argument.
  *
@@ -55,20 +62,20 @@ type SchemaFunction<T> = (props: T & { extraClassNames?: ClassName }) => string
  *
  * ```ts
  *
- *	// Define a schema based on props
- *	const getClassName = classNameManager(props => ({
- *		base: 'base-class',
- *		dynamicVariants: {
- *			variant: {
- *				DEFAULT: 'primary' // Point to the primary variant by default
- *				primary: 'primary-variant',
- *				secondary: 'secondary-variant',
- *			}
- *		},
- *		dynamicClassNames: {
- *			'is-active': props.isActive
- *		}
- * 	}))
+ * // Define a schema based on props
+ * const getClassName = classNameManager(props => ({
+ *  base: 'base-class',
+ *  dynamicVariants: {
+ *   variant: {
+ *    DEFAULT: 'primary' // Point to the primary variant by default
+ *    primary: 'primary-variant',
+ *    secondary: 'secondary-variant',
+ *   }
+ *  },
+ *  dynamicClassNames: {
+ *   'is-active': props.isActive
+ *  }
+ *  }))
  *
  * const buttonClassName = getClassName({ variant: 'secondary', isActive: true })
  *
@@ -77,8 +84,8 @@ type SchemaFunction<T> = (props: T & { extraClassNames?: ClassName }) => string
  *
  * ```
  */
-export function classNameManager <T> (
-	schemaInitializer: (props: T) => Schema<T>
+export function classNameManager <T extends Record<string, unknown>> (
+  schemaInitializer: (props: T) => Schema<T>
 ): SchemaFunction<T>
 
 
@@ -93,17 +100,17 @@ export function classNameManager <T> (
  *
  * ```ts
  *
- *	// Define a schema based on props
- *	const getClassName = classNameManager({
- *		base: 'base-class',
- *		dynamicVariants: {
- *			styleType: {
- *				DEFAULT: 'primary' // Point to the primary variant by default
- *				primary: 'primary-variant',
- *				secondary: 'secondary-variant',
- *			}
- *		},
- *	})
+ * // Define a schema based on props
+ * const getClassName = classNameManager({
+ *  base: 'base-class',
+ *  dynamicVariants: {
+ *   styleType: {
+ *    DEFAULT: 'primary' // Point to the primary variant by default
+ *    primary: 'primary-variant',
+ *    secondary: 'secondary-variant',
+ *   }
+ *  },
+ * })
  *
  * const buttonClassName = getClassName({ styleType: 'secondary', isActive: true })
  *
@@ -113,7 +120,7 @@ export function classNameManager <T> (
  * ```
  */
 export function classNameManager <T extends Record<string, unknown>> (
-	schema: Schema<T>
+  schema: Schema<T>
 ): SchemaFunction<T>
 
 
@@ -123,37 +130,37 @@ export function classNameManager <T extends Record<string, unknown>> (
 /**
  * @internal
  */
-export function classNameManager <T> (
-	schemaOrFunc: ((props: T) => Schema<T>) | Schema<T> = {} as unknown as ((props: T) => Schema<T>) | Schema<T>,
+export function classNameManager <T extends Record<string, unknown>> (
+  schemaOrFunc: ((props: T) => Schema<T>) | Schema<T> = {} as unknown as ((props: T) => Schema<T>) | Schema<T>,
 ): SchemaFunction<T> {
-	return (componentProps: any) => {
-		const schemaObject = typeof schemaOrFunc === 'function'
-			? schemaOrFunc(componentProps)
-			: schemaOrFunc
+  return (componentProps: any) => {
+    const schemaObject = typeof schemaOrFunc === 'function'
+      ? schemaOrFunc(componentProps)
+      : schemaOrFunc
 
-		const { extraClassNames } = componentProps
-		const { base, dynamicVariants, dynamicClassNames } = schemaObject
+    const { extraClassNames } = componentProps
+    const { base, dynamicVariants, dynamicClassNames } = schemaObject
 
-		const classNameArray = [base ?? '']
+    const classNameArray = [base ?? '']
 
-		if (dynamicVariants) {
-			Object.entries(dynamicVariants).forEach(([key, value]: any) => {
-				const propValue = componentProps[key]
-				const schemaValue = value[propValue] ?? value[value.DEFAULT]
-				schemaValue && classNameArray.push(schemaValue)
-			})
-		}
+    if (dynamicVariants) {
+      Object.entries(dynamicVariants).forEach(([key, value]: any) => {
+        const propValue = componentProps[key]
+        const schemaValue = value[propValue] ?? value[value.DEFAULT]
+        schemaValue && classNameArray.push(schemaValue)
+      })
+    }
 
-		if (dynamicClassNames) {
-			Object.entries(dynamicClassNames).forEach(([key, value]) => {
-				value && classNameArray.push(key)
-			})
-		}
+    if (dynamicClassNames) {
+      Object.entries(dynamicClassNames).forEach(([key, value]) => {
+        value && classNameArray.push(key)
+      })
+    }
 
-		if (extraClassNames) classNameArray.push(extraClassNames)
+    if (extraClassNames) classNameArray.push(extraClassNames)
 
-		return classNameArray.flat(4).join(' ').trim()
-	}
+    return classNameArray.flat(4).join(' ').trim()
+  }
 }
 
 export const cnm = classNameManager
